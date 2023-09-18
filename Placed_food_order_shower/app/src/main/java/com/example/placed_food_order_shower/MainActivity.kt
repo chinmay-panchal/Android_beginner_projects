328ecbd90c342fabcd0d42f9d3c019f4b3bd6d43
Chinmay Panchal <Chinmaypanchalcollab@gmail.com>
2023-09-18 11:52:34 +0530

    Resolved errors of binding in the app

--- Placed_food_order_shower/app/src/main/java/com/example/placed_food_order_shower/MainActivity.kt
+++ Placed_food_order_shower/app/src/main/java/com/example/placed_food_order_shower/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.placed_food_order_shower
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.placed_food_order_shower.databinding.ActivityMainBinding
 
@@ -11,7 +13,8 @@ import com.example.placed_food_order_shower.databinding.ActivityMainBinding
 //import kotlinx.android.synthetic.main.activity_main.eT3
 //import kotlinx.android.synthetic.main.activity_main.eT4
 
-private lateinit var binding: ActivityMainBinding
+@SuppressLint("StaticFieldLeak")
+private var binding: ActivityMainBinding? = null
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,13 +22,33 @@ class MainActivity : AppCompatActivity() {
         const val KEY = "com.example.placed_food_order_shower"
     }
 
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//
+//        val btnorder = findViewById<Button>(R.id.btnOrder)
+//        btnorder.setOnClickListener {
+//            val ordersPlaced=
+//                "${binding?.eT1?.text.toString()} ${binding?.eT2?.text.toString()} ${binding?.eT3?.text.toString()} ${binding?.eT4?.text.toString()}"
+//
+//            intent = Intent(this, Order::class.java)
+//            intent.putExtra(KEY, ordersPlaced)
+//            startActivity(intent)
+//        }
+//    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        binding.btnOrder.setOnClickListener {
-            val ordersPlaced= binding.eT1.text.toString() + " " + binding.eT2.text.toString() + " "+
-                    binding.eT3.text.toString() + " " + binding.eT4.text.toString()
+        // binding ko inititalize kardiya
+        /* When you attempt to access the EditTexts using binding?.eT1?.text.toString(), it tries to access eT1 on a null object, which leads to null values.*/
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+//        val btnorder = findViewById<Button>(R.id.btnOrder)
+
+        binding?.btnOrder?.setOnClickListener {
+            val ordersPlaced = "${binding?.eT1?.text.toString()} ${binding?.eT2?.text.toString()} ${binding?.eT3?.text.toString()} ${binding?.eT4?.text.toString()}"
 
             intent = Intent(this, Order::class.java)
             intent.putExtra(KEY, ordersPlaced)
