f66ace84c6a1b16fdb250f5e290f10565c1cfa57
Chinmay Panchal <Chinmaypanchalcollab@gmail.com>
2024-04-12 16:53:14 +0530

    navigation drawer app

---
+++ x_navigation_drawer_clone/app/src/main/java/com/example/x_navigation_drawer_clone/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.x_navigation_drawer_clone.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
